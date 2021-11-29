ee6f8aa3786052b35c663b82f530c04542df46e3
student <maxfrantzen@gmx.de>
2021-11-29 14:02:55 +0100

    Clean code base

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,6 +1,7 @@
+# definition of fixed parameters
 import math
 import rospy
-# definition of fixed parameters
+
 OFFSETS = [1., 0.6, 1.4, 0.8, 1.6]
 SPIN_TIME = 15
 SPIN_TIME_SELECT_SEARCH = rospy.Duration(secs=45)
